64efabe9d705060a66c8bb934f099e9a8e4daaa2
JinyaoZhu <jinyao383@hotmail.com>
2018-12-09 01:17:56 +0100

    Add animation.

---
+++ Assets/Scripts/SetScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Disable screen dimming
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
