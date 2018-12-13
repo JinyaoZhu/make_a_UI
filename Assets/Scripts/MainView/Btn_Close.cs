fee3d4c9b54fe82918a61ba8f817b95183db0c75
Xuehua Jia <33216835+Xeuhua@users.noreply.github.com>
2018-12-14 00:49:06 +0100

    new Setting Button

---
+++ Assets/Scripts/MainView/Btn_Close.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Btn_Close : MonoBehaviour {
+
+public void Quit()
+    {
+        Debug.Log("has quit the App");
+            Application.Quit();
+
+    }
+}
