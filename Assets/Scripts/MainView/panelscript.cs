b635003df5638063f0fa19c3b8a11ed2c1569972
Xuehua Jia <33216835+Xeuhua@users.noreply.github.com>
2018-12-13 10:12:41 +0100

    New Fonts and Setting Button
    new

---
+++ Assets/Scripts/MainView/panelscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class panelscript : MonoBehaviour {
+
+    public GameObject Optionsmenu;
+    int counter;
+    public void showhidePanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            Optionsmenu.gameObject.SetActive(false);
+           
+        } 
+        else
+        {
+            Optionsmenu.gameObject.SetActive(true);
+
+        }
+       
+    }
+	
+}
