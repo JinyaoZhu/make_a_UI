649ed9e7c24d03ed962cd205021102de8d700f5d
JinyaoZhu <jinyao383@hotmail.com>
2019-01-08 23:02:22 +0100

    [DetailView] Add power button.

---
+++ Assets/Scripts/DetailView/PowerSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerSlider : MonoBehaviour {
+
+	public Slider slider;
+	public GameObject sliderHandle;
+
+	bool is_button_pressed;
+
+	// Use this for initialization
+	void Start () {
+		// powerButton.OnPointerUp.AddListener (onButtonReleased);
+		// powerButton.OnPointerDown.AddListener (onButtonPressed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		is_button_pressed = sliderHandle.GetComponent<PowerButtonState>().buttonPressed;
+		Debug.Log(is_button_pressed);
+		if (!is_button_pressed) {
+			if ((slider.value > 0.5f) && (slider.value < 0.95f)) slider.value += 0.05f;
+
+			if (slider.value > 0.05f && slider.value<=0.5f) slider.value -= 0.05f;
+
+			if(slider.value >= 0.95f) slider.value= 1.0f;
+
+			if(slider.value <= 0.05f) slider.value= 0.0f;
+		}
+	}
+}
