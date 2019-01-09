72078a7203bcbfdd4f2f30cc0abb969c55431ee0
JinyaoZhu <jinyao383@hotmail.com>
2019-01-09 16:23:43 +0100

    Update ModelRotator.cs

--- Assets/Scripts/DetailView/ModelRotator.cs
+++ Assets/Scripts/DetailView/ModelRotator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
+public class ModelRotator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
