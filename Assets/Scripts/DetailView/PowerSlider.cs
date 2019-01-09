c7407d3fac5350220561cec31be81de1970fc54f
JinyaoZhu <jinyao383@hotmail.com>
2019-01-09 15:38:43 +0100

    [DetailView] Add value bars.

--- Assets/Scripts/DetailView/PowerSlider.cs
+++ Assets/Scripts/DetailView/PowerSlider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,17 +12,22 @@ public class PowerSlider : MonoBehaviour {
 	public Button powerButton;
 
 	bool is_button_pressed;
+	float lastSliderValue;
 
 	// Use this for initialization
 	void Start () {
 		// powerButton.OnPointerUp.AddListener (onButtonReleased);
 		// powerButton.OnPointerDown.AddListener (onButtonPressed);
+		lastSliderValue = slider.value;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		is_button_pressed = sliderHandle.GetComponent<PowerButtonState> ().buttonPressed;
-		Debug.Log (is_button_pressed);
+		
+        if(Math.Abs(lastSliderValue - slider.value) > 0.5) slider.value = lastSliderValue;
+		lastSliderValue = slider.value;
+
 		if (!is_button_pressed) {
 			if ((slider.value > 0.5f) && (slider.value < 0.95f)) slider.value += 0.05f;
 
@@ -30,13 +36,13 @@ public class PowerSlider : MonoBehaviour {
 			if (slider.value >= 0.95f) {
 				slider.value = 1.0f;
 				text.text = "POWER IS ON";
-				powerButton.GetComponent<Image> ().color = Color.green;
+				powerButton.GetComponent<Image> ().color = new Color(0.0f,0.9f,0.0f, 0.8f);
 			}
 
 			if (slider.value <= 0.05f) {
 				slider.value = 0.0f;
 				text.text = "SWAP TO POWER ON";
-				powerButton.GetComponent<Image> ().color = Color.red;
+				powerButton.GetComponent<Image> ().color = new Color(0.9f,0.0f,0.0f, 0.8f);
 			}
 		}
 	}
