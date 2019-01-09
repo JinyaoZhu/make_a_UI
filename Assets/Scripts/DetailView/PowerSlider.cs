8dba5a636045097016c5cb4eb73e71ac8cce65f6
JinyaoZhu <jinyao383@hotmail.com>
2019-01-09 11:14:32 +0100

    [DetailView] Add settingSlider.

--- Assets/Scripts/DetailView/PowerSlider.cs
+++ Assets/Scripts/DetailView/PowerSlider.cs
@@ -7,6 +7,8 @@ public class PowerSlider : MonoBehaviour {
 
 	public Slider slider;
 	public GameObject sliderHandle;
+	public Text text;
+	public Button powerButton;
 
 	bool is_button_pressed;
 
@@ -18,16 +20,24 @@ public class PowerSlider : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		is_button_pressed = sliderHandle.GetComponent<PowerButtonState>().buttonPressed;
-		Debug.Log(is_button_pressed);
+		is_button_pressed = sliderHandle.GetComponent<PowerButtonState> ().buttonPressed;
+		Debug.Log (is_button_pressed);
 		if (!is_button_pressed) {
 			if ((slider.value > 0.5f) && (slider.value < 0.95f)) slider.value += 0.05f;
 
-			if (slider.value > 0.05f && slider.value<=0.5f) slider.value -= 0.05f;
+			if (slider.value > 0.05f && slider.value <= 0.5f) slider.value -= 0.05f;
 
-			if(slider.value >= 0.95f) slider.value= 1.0f;
+			if (slider.value >= 0.95f) {
+				slider.value = 1.0f;
+				text.text = "POWER IS ON";
+				powerButton.GetComponent<Image> ().color = Color.green;
+			}
 
-			if(slider.value <= 0.05f) slider.value= 0.0f;
+			if (slider.value <= 0.05f) {
+				slider.value = 0.0f;
+				text.text = "SWAP TO POWER ON";
+				powerButton.GetComponent<Image> ().color = Color.red;
+			}
 		}
 	}
 }
