aed4bfa0423b73c8044ae749c5ef0a77ebaeb63f
gao yue <1319201443@qq.com>
2019-01-18 18:58:41 +0100

    Search bar

--- Assets/Scripts/SearchView/ButtonTest.cs
+++ Assets/Scripts/SearchView/ButtonTest.cs
@@ -7,6 +7,7 @@ public class ButtonTest : MonoBehaviour
 {
     List<GameObject> messages = new List<GameObject>();
     public GameObject item;
+    public GameObject obj;
     GameObject myMessage;
     GameObject parent;
     Vector3 itemLocalPos;
@@ -14,8 +15,13 @@ public class ButtonTest : MonoBehaviour
     float itemHeight;
 
     int count = 0;
+    bool showall = true;
+    bool showone = true;
+    bool comparedIs = false;
+
     List<Transform> allnameslist = new List<Transform>();
-    string inputtext = "";
+    string inputtext_new = "";
+    string inputtext_alt = "";
 
 
     void Start()
@@ -41,6 +47,13 @@ public class ButtonTest : MonoBehaviour
                 
             }
         }
+        if (inputtext_alt == "")
+        {
+            for (int j = 0; j < allnameslist.Count; j++)
+            {
+                AddItem(allnameslist[j].name);
+            }
+        }
 
     }
     void Update()
@@ -49,99 +62,131 @@ public class ButtonTest : MonoBehaviour
         count = 0;
         //Grid的长度随着生成物体个数变化
         // this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(this.gameObject.GetComponent<RectTransform>().sizeDelta.x, 0);
-        inputtext = GameObject.Find("SearchView").transform.Find("MainArea/SearchBar/Text").GetComponent<Text>().text;
+        inputtext_new= GameObject.Find("SearchView").transform.Find("MainArea/SearchBar/Text").GetComponent<Text>().text;
 
-        ///-------------------------------------------添加列表清空代码，实现实时更新及结束。
-        compared();
+
+        if (inputtext_alt != inputtext_new)
+        {
+            inputtext_alt = inputtext_new;
+            compared();
+        }
+        else
+        {
+            inputtext_alt = inputtext_new;
+        }
+          
     }
 
     void compared()
     {
+        // obj = GameObject.Find("SearchView/MainArea/ShowField/Scroll View/Viewport/Content");
 
-        //if (inputtext == "")
-        //{
-        //    for (int j = 0; j < allnameslist.Count; j++)
-        //    {
-        //        AddItem(allnameslist[j].name);
-        //    }
-        //}
+        //obj = GameObject.Find("SearchView").transform.Find("MainArea/ShowField/Scroll View/Viewport").gameObject;
+        obj = GameObject.Find("Content");
+        if (obj == null)
+        {
+            Debug.Log("找不到");
+        }
+        int childs = obj.transform.childCount;
+
+        Debug.Log(childs);
+
+        for (int i = childs-1;i>=0;i--)
+        {
+            GameObject.DestroyImmediate(obj.transform.GetChild(i).gameObject);
+        }
+
+
+
+
+        if (inputtext_new == "")
+        {
+            for (int j = 0; j < allnameslist.Count; j++)
+            {
+                // showall = false;
+                //showone = true;
+                AddItem(allnameslist[j].name);
+            }
+        }
+
+
         for (int i = 0; i < allnameslist.Count; i++)
-        {
-
-
-            //Debug.Log("list ：" + allnameslist[i].name);
-
-            //强制大写转换
-            inputtext = inputtext.ToString().ToUpper();
-
-            if (inputtext != "" && allnameslist[i].name.Contains(inputtext))
             {
-                Debug.Log("include" + "String：" + allnameslist[i]);
+                Debug.Log("单元个数" + allnameslist.Count);
 
+                //强制大写转换
+                inputtext_new = inputtext_new.ToString().ToUpper();
 
-                AddItem(allnameslist[i].name);//生成列表
+                if (inputtext_new != "" && allnameslist[i].name.Contains(inputtext_new) )
+                {
+                    Debug.Log("include" + "String：" + allnameslist[i]);
+
+                    showall = true;
+                    //showone = false;
+                    AddItem(allnameslist[i].name);//生成列表
+                    if (i == allnameslist.Count)
+                    {
+                        showone = false;
+                    }
+
+                }
+                else if (inputtext_new != "" && (allnameslist[i].name.Contains(inputtext_new) == false) )
+                {
+                    count = count + 1;
+                    // showone = false;
+                    Debug.Log("not include");
+                }
+
             }
-            else if (inputtext != "" && (allnameslist[i].name.Contains(inputtext) == false))
+
+            if (count == allnameslist.Count)
             {
-                count = count + 1;
-
-                Debug.Log("not include");
+                
+               AddItem("Cannot find component!");
             }
-
-        }
-        if (count == allnameslist.Count)
-        {
-           AddItem("Cannot find component!");
-        }
-
     }
     //添加列表项
     public void AddItem(string Thema)
     {
 
         GameObject a = Instantiate(item) as GameObject;
-        
+
         a.transform.Find("Text").GetComponent<Text>().text = Thema;
+
         a.GetComponent<Button>().onClick.AddListener(
-        delegate () {
+       delegate () {
 
-                Debug.Log("按下按钮");
-                
-            }
-        );
-
-        /// Debug.Log("按不到按钮");
+           Debug.Log("按下按钮");
+            // RemoveItem(a);
+        }
+       );
 
         a.GetComponent<Transform>().SetParent(parent.GetComponent<Transform>(), false);
-        a.transform.localPosition = new Vector3(itemLocalPos.x, itemLocalPos.y - allnameslist.Count * itemHeight, 0);
-        //messages.Add(a);
+        a.transform.localPosition = new Vector3(itemLocalPos.x, itemLocalPos.y - messages.Count * itemHeight, 0);
+        messages.Add(a);
+        
 
         if (contentSize.y <= allnameslist.Count * itemHeight)//增加内容的高度
         {
-            parent.GetComponent<RectTransform>().sizeDelta = new Vector2(contentSize.x, allnameslist.Count * itemHeight);
+            parent.GetComponent<RectTransform>().sizeDelta = new Vector2(contentSize.x, messages.Count * itemHeight);
         }
     }
 
     // -----------------在此处写成函数
-    //public void RemoveItem(GameObject t)
-    //{
+    public void RemoveItem(GameObject t)
+    {
 
-    //    Debug.Log("按钮被按住");
+        if(comparedIs)
+        {
 
-    //    int index = messages.IndexOf(t);
-    //    messages.Remove(t);
-    //    // Destroy(t);
-
-    //    //for (int i = index; i < messages.Count; i++)//移除的列表项后的每一项都向前移动
-    //    //{
-    //    //    messages[i].transform.localPosition += new Vector3(0, itemHeight, 0);
-    //    //}
-
-    //    //if (contentSize.y <= messages.Count * itemHeight)//调整内容的高度
-    //    //    parent.GetComponent<RectTransform>().sizeDelta = new Vector2(contentSize.x, messages.Count * itemHeight);
-    //    //else
-    //    //    parent.GetComponent<RectTransform>().sizeDelta = contentSize;
-    //}
+            // obj = GameObject.Find("SearchView").transform.Find("MainArea/Scroll View/Viewport/Content");
+            obj = GameObject.Find("SearchView/MainArea/Scroll View/Viewport/Content");
+            foreach (Transform child in obj.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
 
 }
 
